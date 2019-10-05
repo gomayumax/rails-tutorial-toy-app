0715df99e439487cb32155ef000d4d03618731d2
gomayumax <gomayumax@gmail.com>
2019-10-05 08:45:40 +0000

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+  validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
+  has_many :tweets
+end
